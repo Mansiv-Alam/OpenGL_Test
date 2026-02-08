b8a44521eb900dcb6867fce35de8090e2479d5b0
Mansiv Alam <mansiv.alam1@gmail.com>
2026-02-07 23:44:27 -0500

    Testing how to create a triangle

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -1,6 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <Windows.h>
+#include <windows.h>
 #include <iostream>
 
 // Callback to resize the viewport when the window size changes
@@ -8,6 +8,16 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
+// Key inputs 
+void processInput(GLFWwindow* window)
+{
+    // Use the glfw get key function (returns GLFW_PRESS or GLFW_RELEASE)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true); // Closes the window if escape is pressed 
+    }
+}
+
 
 
 int main()
@@ -45,10 +55,39 @@ int main()
         return -1;
     }
 
+    // Making 3 verticies (z coordinate is 0)
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
     // Binding object tests
     unsigned int VAO; // Create the object
     glGenVertexArrays(1, &VAO); // Store the reference to the Object as an ID
     glBindVertexArray(VAO); // Binds the Object as the current active object in the OpenGL context
+
+    // Make a VBO (Vertex Buffer Object) to send all the vertex data at once
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // VBO buffer type is an array buffer (binds the array data to the GL_ARRAY_BUFFER target)
+    // Copies the vertex data in to the current bound buffer, 
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // GL_ARRAY_BUFFER = target buffer, sizeof(vertices) = size of the data, vertices = data to transfer, GL_STATIC_DRAW = how to use the data 
+    // GL_STREAM_DRAW = data is set once and used a few times, GL_STATIC_DRAW = data is set once and used many times, GL_DYNAMIC_DRAW = data is changed alot and used many times
+
+    // Write the vector shader 
+    const char* vertexShaderSource = "#version 330 core\n" // Use GLSL 3.3 (the shader language for OpenGL 3.3)
+        "layout (location = 0) in vec3 aPos;\n" // declare an input variable as aPos which is a vec3 or 3d vector type  
+        "void main()"
+        "{\n"
+        " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" // Tells the gpu where to place the vector onto the screen, must cast to 4d vector, gl_Position is required in every vector shader
+        "}\0";
+    // Create and compile a shader object 
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER); // Indicate the type of vector it is
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // argument 1 = shader to compile, argument 2 = number of strings passed through as source code, argument 3 = source code of the vector shader, 
+    // argument 4 = length of the source code (null here because we want the function to read till the end of the source code)
+    glCompileShader(vertexShader);
 
     // Set viewport and resize callback
     glViewport(0, 0, 800, 600);
@@ -56,7 +95,7 @@ int main()
 
     glBindVertexArray(0); // optional unbind (sets the object id of the window target to 0)
 
-    // Main loop
+    // Render loop
     while (!glfwWindowShouldClose(window))
     {
         float red = 121.0 / 255.0;
@@ -64,10 +103,10 @@ int main()
         float blue =  199.0 / 255.0;
 
         // Clear the screen with a specified color
-        glClearColor(red, green, blue, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(red, green, blue, 1.0f); // State-setting function
+        glClear(GL_COLOR_BUFFER_BIT); // State-using function
 
-
+        processInput(window);
 
         // Swap buffers and poll for events
         glfwSwapBuffers(window); // Puts the drawn screen onto the main screen (back buffer > front buffer)
