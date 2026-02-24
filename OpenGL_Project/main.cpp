970b9eb87fa79971368a38c6faef133a6efba389
Mansiv Alam <mansiv.alam1@gmail.com>
2026-02-23 22:45:36 -0500

    test manual translation and rotation matrices (x, y, z axes)

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -256,13 +256,55 @@ int main()
     std::cout << result[0][0] << "," << result[0][1] << "\n" << result[1][0] << "," << result[1][1] << std::endl;
     
     // Scaling
-    int vec4[] = { 3, 2, 1, 1};
+    int vec4[] = { 3, 2, 1, 1}; // w = Homogeneous coordinate, when it is 0 the vector becomes a direction vector (translations are not possible)
     float MatrixScale[4][4] = { {1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1} }; // identity matrix
     float resultf[4];
-    MatrixScale[0][0] = 0.5; // Non uniform scale (scaling factor is different for each axis)
+    MatrixScale[0][0] = 3; // Non uniform scale (scaling factor is different for each axis)
     MatrixScale[1][1] = 2;
-    MatrixScale[2][2] = 1;
+    MatrixScale[2][2] = 2;
     MatrixScale[3][3] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << MatrixScale[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    // Translation
+    MatrixScale[0][3] = 2;
+    MatrixScale[1][3] = 2;
+    MatrixScale[2][3] = 2;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << MatrixScale[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    // Rotation Around the X axis (in radians) (90 is the angle you want to rotate the vector by on that axis)
+    MatrixScale[0][3] = 0; MatrixScale[1][3] = 0; MatrixScale[2][3] = 0;
+    MatrixScale[1][1] = cos(90); MatrixScale[1][2] = -sin(90); 
+    MatrixScale[2][1] = sin(90); MatrixScale[2][2] = cos(90);
+    // Rotation Around the Y axis (in radians)
+    MatrixScale[1][1] = 1; MatrixScale[1][2] = 0; MatrixScale[2][1] = 0; // Reset values
+    MatrixScale[0][0] = cos(90); MatrixScale[0][2] = sin(90);
+    MatrixScale[2][0] = -sin(90); MatrixScale[2][2] = cos(90);
+    // Rotation Around the Z axis (in radians)
+    MatrixScale[2][0] = 0; MatrixScale[2][2] = 1; MatrixScale[0][0] = 1; MatrixScale[0][2] = 0;// Reset values
+    MatrixScale[0][0] = cos(90); MatrixScale[0][1] = -sin(90);
+    MatrixScale[1][0] = sin(90); MatrixScale[1][1] = cos(90);
+    // Careful of combining the rotation matrices due to Gimbal Locks, use Rodrigues' Rotation Matrix or Quaternions for complete avoidance
+    // Also Matrix Multiplication is not commutative, so order matters (multiplying the translation then the scale would also scale the translation vector)
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << MatrixScale[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
 
     // i < N
     for (int i = 0; i < 4; i++) {
