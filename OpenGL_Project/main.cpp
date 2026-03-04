3b87dfc7de2d3dbbc43f380fdb38fe1f2568f60f
Mansiv Alam <mansiv.alam1@gmail.com>
2026-03-03 21:10:04 -0500

    Implement orbiting camera around origin using lookAt

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -386,8 +386,8 @@ int main()
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 1.0f)); // Rotates or changes the position in the world space
     
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // distance from the camera
+    //glm::mat4 view = glm::mat4(1.0f);
+    //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // distance from the camera
 
 
     // Orthographic projection 
@@ -448,6 +448,24 @@ int main()
 
             glDrawArrays(GL_TRIANGLES, 0, 36); // Draw Cube
         }
+
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget); // Reverse direction (direction from the origin to the camera)
+
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection)); // Cross product, points to the positive x axis
+        glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight); // Cross product of the right vector to get the up vector
+
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+
+        cameraPos = glm::vec3(camX, 0.0f, camZ);
+
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraTarget, cameraUp); // Creates a view matrix for the camera system
 
         shader.setMat4("view", view);
         shader.setMat4("projection", proj);
