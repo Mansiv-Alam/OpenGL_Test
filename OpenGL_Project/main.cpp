1dba216f2ce4fcf668a1100a266f03a212a1e8a4
Mansiv Alam <mansiv.alam1@gmail.com>
2026-03-06 23:12:17 -0500

    Implement mouse camera rotation (pitch/yaw) and scroll zoom

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -10,12 +10,24 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Timing
+
+float deltaTime = 0.0f; // Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+// Camera 
+
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-float deltaTime = 0.0f; // Time between current frame and last frame
-float lastFrame = 0.0f; // Time of last frame
+bool firstMouse = true;
+float yaw = -90.0f, pitch = 0.0f;
+float lastX = SCR_WIDTH/2.0f, lastY = SCR_HEIGHT / 2.0f;
+float fov = 45.0f;
 
 // Callback to resize the viewport when the window size changes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -48,6 +60,56 @@ void processInput(GLFWwindow* window)
 
 }
 
+// Mouse Input
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    // No jitter based off where the mouse cursor came from
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    // Find the offset from the last cursor position
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    // change the offset using a sensitivity
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    // Change the pitch and yaw using the offset
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // Clamp the pitch so the camera doesn't turn upside down (90 degrees = parallel vectors = 0 cross product = broken camera)
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+
+    // Change the fov depending on the vertical scroll
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+}
+
 int main()
 {
     HWND consoleWindow = GetConsoleWindow();
@@ -66,15 +128,22 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Testing", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL Testing", NULL, NULL);
     if (window == NULL)
     {
         //std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
-
+    
+    // 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // Initialize GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -273,7 +342,6 @@ int main()
 
     // Set viewport and resize callback
     glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     shader.use();
     shader.setInt("texture2", 1); // use the shader class to set the uniforms 
@@ -415,7 +483,7 @@ int main()
     // Orthographic projection 
     //glm::mat4 proj = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
     // Perspective projection
-    glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f); // 45 degrees for fov, aspect ratio (width/height), near distance/plane, far distance/plane
+    glm::mat4 proj = glm::perspective(glm::radians(fov), (float)SCR_WIDTH/ (float)SCR_HEIGHT, 0.1f, 100.0f); // fov dependent on the user, aspect ratio (width/height), near distance/plane, far distance/plane
 
 
     
@@ -483,6 +551,8 @@ int main()
 
         glm::mat4 view;
         view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); // Creates a view matrix for the camera system
+
+        glm::mat4 proj = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); // fov dependent on the user, aspect ratio (width/height), near distance/plane, far distance/plane
 
         shader.setMat4("view", view);
         shader.setMat4("projection", proj);
